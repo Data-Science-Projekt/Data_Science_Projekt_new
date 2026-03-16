35f19e060e88290662db52cfdbd20044f7f81826
BMakko <balduinkiel@gmail.com>
2026-03-16 15:56:32 +0100

    Update risikomanagement.py

--- risikomanagement.py
+++ risikomanagement.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 import requests
 
 # --- CONFIG ---
-AV_API_KEY = st.secrets["AV_API_KEY"]
+AV_API_KEY = st.secrets.get("AV_API_KEY", "")
 STOCKS = {"Apple": "AAPL", "NVIDIA": "NVDA"}
 
 
