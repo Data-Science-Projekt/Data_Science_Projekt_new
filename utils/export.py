7ad7534f1b58897611a027a42292a983528765ee
2Fame-coder <jakob.puchert@gmail.com>
2026-03-18 11:00:03 +0100

    Fix: größere Titel, Achsen und Legende näher am Graph

--- utils/export.py
+++ utils/export.py
@@ -10,37 +10,37 @@ def fig_to_pdf_bytes(fig, width=2480, height=1654, scale=1):
     fig_export = copy.deepcopy(fig)
 
     fig_export.update_layout(
-        font=dict(size=36, color="black"),
-        title_font=dict(size=42, color="black"),
+        font=dict(size=42, color="black"),
+        title_font=dict(size=56, color="black"),
         paper_bgcolor="white",
         plot_bgcolor="white",
-        margin=dict(l=160, r=80, t=80, b=200),
+        margin=dict(l=180, r=80, t=120, b=220),
         legend=dict(
-            font=dict(size=34, color="black"),
-            bgcolor="white",
+            font=dict(size=40, color="black"),
+            bgcolor="rgba(255,255,255,0.9)",
             bordercolor="black",
             borderwidth=2,
             orientation="h",
             yanchor="bottom",
-            y=-0.28,
+            y=-0.22,  # näher am Graph
             xanchor="center",
             x=0.5
         ),
         xaxis=dict(
-            title_font=dict(size=38, color="black"),
-            tickfont=dict(size=32, color="black"),
+            title_font=dict(size=48, color="black"),
+            tickfont=dict(size=38, color="black"),
             linecolor="black",
             linewidth=2,
             gridcolor="#dddddd",
-            title_standoff=25,
+            title_standoff=20,
         ),
         yaxis=dict(
-            title_font=dict(size=38, color="black"),
-            tickfont=dict(size=32, color="black"),
+            title_font=dict(size=48, color="black"),
+            tickfont=dict(size=38, color="black"),
             linecolor="black",
             linewidth=2,
             gridcolor="#dddddd",
-            title_standoff=25,
+            title_standoff=20,
         ),
     )
 
